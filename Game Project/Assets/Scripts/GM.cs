dccd58be543959d7e87036ca630951432d7c0273
Bart Scholten <bwrjscholten@gmail.com>
2020-01-15 15:52:24 +0100

    Game project
    Arduino + Unity Code

---
+++ Game Project/Assets/Scripts/GM.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.IO.Ports;
+
+public class GM : MonoBehaviour
+{
+
+    public static float vertVel = 0;
+    public static float timeTotal = 0;
+
+    public static float zVelAdj = 1;
+
+    public static string lvlCompStatus = "";
+    public float waittoload = 0;
+
+    public float zScenePos = 110;
+
+    public Transform BuildingBlock;
+    public Transform obstObject;
+
+    public int randNumb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(BuildingBlock, new Vector3(0, 4.138f, 94), BuildingBlock.rotation);
+        Instantiate(BuildingBlock, new Vector3(0, 4.138f, 98), BuildingBlock.rotation);
+        Instantiate(BuildingBlock, new Vector3(0, 4.138f, 102), BuildingBlock.rotation);
+        Instantiate(BuildingBlock, new Vector3(0, 4.138f, 106), BuildingBlock.rotation);
+
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (zScenePos < 130)
+        {
+            Instantiate(BuildingBlock, new Vector3(0, 4.138f, zScenePos), BuildingBlock.rotation);
+            zScenePos += 4;
+
+            
+        }
+
+
+        if (randNumb < 2)
+        {
+            Instantiate(obstObject, new Vector3(-1, 5.138f, zScenePos), obstObject.rotation);
+        }
+        if (randNumb == 4)
+        {
+            Instantiate(obstObject, new Vector3(0, 5.138f, zScenePos), obstObject.rotation);
+        }
+        if (randNumb > 8)
+        {
+            Instantiate(obstObject, new Vector3(1, 5.138f, zScenePos), obstObject.rotation);
+        }
+
+        Instantiate(BuildingBlock, new Vector3(0, 4.138f, zScenePos), BuildingBlock.rotation);
+        zScenePos += 4;
+
+        randNumb = Random.Range(0, 10);
+
+        timeTotal += Time.deltaTime;
+
+        if (lvlCompStatus == "Fail")
+        {
+            waittoload += Time.deltaTime;
+        }
+        if (waittoload > 2)
+        {
+            SceneManager.LoadScene("LevelComp");
+        }
+    }
+}
---
+++ Game Project/Assets/Scripts/moveplayer.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.IO.Ports;
+
+public class moveplayer : MonoBehaviour
+{
+    SerialPort sp = new SerialPort("COM3", 9600);
+
+    
+
+    public KeyCode moveL;
+    public KeyCode moveR;
+    public KeyCode moveUp;
+    public KeyCode moveDown;
+
+    public float horizVelo = 0;
+    public int laneNum = 2;
+    public string controlLocked = "n";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sp.Open();
+        sp.ReadTimeout = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Rigidbody>().velocity = new Vector3(horizVelo, GM.vertVel, 4);
+
+        if ((Input.GetKeyDown(moveL)) && (laneNum>1) && (controlLocked == "n"))
+        {
+            horizVelo = -2;
+            StartCoroutine(stopSlide());
+            laneNum -= 1;
+            controlLocked = "y";
+        };
+
+        if ((Input.GetKeyDown(moveR)) && (laneNum<3) && (controlLocked == "n"))
+        {
+            horizVelo = 2;
+            StartCoroutine(stopSlide());
+            laneNum += 1;
+            controlLocked = "y";
+        };
+
+        if (sp.IsOpen)
+        {
+            if ((sp.ReadByte() == 1) && (laneNum > 1) && (controlLocked == "n"))
+            {
+                horizVelo = -2;
+                StartCoroutine(stopSlide());
+                laneNum -= 1;
+                controlLocked = "y";
+
+            };
+
+            if ((sp.ReadByte() == 2) && (laneNum < 3) && (controlLocked == "n"))
+            {
+                horizVelo = 2;
+                StartCoroutine(stopSlide());
+                laneNum += 1;
+                controlLocked = "y";
+            };
+
+        }
+
+
+    }
+
+    void OnCollisionEnter (Collision other)
+    {
+        if (other.gameObject.tag == "lethal")
+        {
+            Destroy(gameObject);
+            GM.zVelAdj = 0;
+            
+            GM.lvlCompStatus = "Fail";
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.name == "RampTrigger")
+        {
+            GM.vertVel = 2;
+        }
+        if (other.gameObject.name == "RampDeTrigger")
+        {
+            GM.vertVel = 0;
+        }
+        if (other.gameObject.name == "exit")
+        {
+            GM.lvlCompStatus = "Succes";
+            SceneManager.LoadScene("LevelComp");
+        }
+    }
+
+    IEnumerator stopSlide()
+    {
+        yield return new WaitForSeconds(.5f);
+        horizVelo = 0;
+        controlLocked = "n";
+    }
+
+    
+}
